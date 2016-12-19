3f732ec54098e05873c226a07642ac339cb0182f
Phelanlewis <phelanlewis@gmail.com>
2016-12-19 13:55:17 -0800

    added apiai to index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var URL = require('url-parse');
+var request =   require('request');
+var cheerio =   require('cheerio');
+var URL     =   require('url-parse');
+var apiai   =   require('apiai');
 
+var app = apiai("798a3a0092d8471b9db20d3e102a331f");
+
+//node webscarpe section
 var pageToVisit = "http://www.usedvictoria.com/";
 
+//testin that it works
 console.log("Visiting page " + pageToVisit);
 
 request(pageToVisit, function(error, response, body) {
@@ -18,3 +23,19 @@ request(pageToVisit, function(error, response, body) {
      console.log("Page title:  " + $('title').text());
    }
 });
+
+//apiai section to test that it works
+
+var request = app.textRequest('<Your text query>', {
+    sessionId: '<unique session id>'
+});
+
+request.on('response', function(response) {
+    console.log(response);
+});
+
+request.on('error', function(error) {
+    console.log(error);
+});
+
+request.end();
